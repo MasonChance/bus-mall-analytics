8d7fd9a3cd07e59e507e2f1d4de371560fe42cbf
MasonChance <mason.fryberger@gmail.com>
2020-04-19 12:37:04 -0700

    second dataset added to chart. localstorage working

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var lastItemsSeen = [];
 
 SurveyItems.productList = [];  
   
-function SurveyItems(product, imageUrl){
+function SurveyItems(product, imageUrl, views=0, clicks=0){
   this.product = product; // used as object element(Id);
   this.imageUrl = imageUrl;
-  this.views = 0;
-  this.clicks = 0;
+  this.views = views;
+  this.clicks = clicks;
    
   SurveyItems.productList.push(this); 
 }
@@ -54,7 +54,7 @@ function validateUnique(threeArray){
   for(var i = 0; i < threeArray.length; i ++){
     for(var v = 0; v < lastItemsSeen.length; v++){
       if(threeArray[i].product === lastItemsSeen[v].product){
-        console.log('check failed regenerate threeArray');
+        
         return false;
         
         }
@@ -72,10 +72,11 @@ function getOneProduct(){
 } 
 
 // CallBack Function Below//
-var totalClicks = 1;
+var totalClicks = 0;//GlobalVariable!!!
+
 function census(e){
-  
   var itemId = e.target.id;
+  
   for(var i = 0; i < SurveyItems.productList.length; i++){
     var name = SurveyItems.productList[i];``
     if(name.product === itemId){
@@ -83,6 +84,7 @@ function census(e){
     }
   }
   var getTarget = document.getElementById('choices');
+
   if(totalClicks <= 4){//TODO: when finished, change count to 25. 
     totalClicks++
     getTarget.innerHTML = '';
@@ -92,11 +94,13 @@ function census(e){
     var thanks = document.createElement('h1');
     thanks.textContent = 'Thank you for your feedback';
     getTarget.appendChild(thanks);
-    showResults();
+    var testGroupResult = JSON.stringify(SurveyItems.productList);
+    localStorage.setItem('productList',testGroupResult);
+    
+    trackResults();
     retrieveAddStore();
 
   }
-
 
 } 
 
@@ -133,62 +137,45 @@ renderToPage();
 var targetArea = document.getElementById('choices');
 targetArea.addEventListener('click', census);
 
-/* TODO: store ea. ItemObject.clicks to local Storage
-   TODO: store ea. ItemObject.veiws to local Storage
+/*
    
    Objective. store total clicks for each sample group of 25. Reset the clicks and views counts per instance of a sample group; while maintaing a running count of the total clicks and views in Locale storage. retrieve these total values across all test groups (instance of 25) and display the overall data values for reference on a seperate chart from the one that displays individual test group results. 
 
    TODO: CLICKS Count  one function. set as property of constructor. only job is to pull initial stored value, parseint(). add to current testGroup results for item, store back in local storage. iterate through all items. 
 
-   TODO: VIEWS: AS FOR CLICKS.  
 
 */
 
-// ====CLICK STORING FUNCTION====//
-//============================// 
-//function below only needs to run if a product is addeded to the array this creates a localStorage key with a value of '0' to later be run when totaling test group results. //FIXME: consider how to tie to constructor but only triger for the object instantiation and not on page refresh or other events. consider that hoisting may provide a self contained solution. 
+// ==== TOTAL CLICK STORING FUNCTION====//
 
-function createLocalKeys(){
-  for(var i = 0; i < SurveyItems.productList.length; i ++){
-   var extractedName = SurveyItems.productList[i].product;
-   localStorage.setItem(extractedName, '0');
-  }
-}
-//Call above function 
-createLocalKeys();
+
 
 //==== extract base value from local, add to current, store result in local. 
 function retrieveAddStore(){
-  for(var i = 0; i < SurveyItems.productList.length; i ++){
-    var storageKey = SurveyItems.productList[i].product;
-    var valueToAdd = SurveyItems.productList[i].clicks;
-    var storedValueString = localStorage.getItem(storageKey);
-    var newTotal = JSON.parse(storedValueString); // typeof still returns value of string. seems to resist all forms of Type Coersion
-    // var totalAsString = newTotal.tostring;
-        // is this the use case for JSON.stringify????/JSON.Parse
+  var updateStoredResults = [];
+  // values retrieved from storage. 
+  var lastGroupResults = JSON.parse(localStorage.getItem('productList'));
+  // values from current group results. 
+  for(var i = 0; i < lastGroupResults.length; i++){
+    var currentResults = SurveyItems.productList;
+    // sums clicks and veiws from last and current.
+    var updateClickResult = (currentResults[i].clicks) + (lastGroupResults[i].clicks);
+    var updateViewsResult = (currentResults[i].views) + (lastGroupResults[i].views);
+    // re-instantiation of object for storage, static variables to pass in. 
+    var name = currentResults[i].product;
+    var img = currentResults[i].imageUrl;
+    updateStoredResults.push(
+      new SurveyItems(name, img, updateViewsResult, updateClickResult )
+    )
+    
   }
-  console.log(typeof storedValueString);
-  console.log(typeof valueToAdd);
-  console.log('storageKey:', storageKey);
-  console.log('valueToAdd: ', valueToAdd);
-  console.log('storedValueString: ', storedValueString);
-  console.log('newTotal: ', newTotal);
-  // console.log('totalAsString: ' + totalAsString);
-
-  // localStorage.setItem(storageKey, totalAsString);
+  localStorage.setItem('productList', updateStoredResults);
+  
 }
-
-retrieveAddStore();
-
-
-
-
-   
+ 
 //========= Chart below ======//
-// change labels to items (by name) of product array. 
-//TODO: Add DataSet for plotting Veiws as a lineGraph on the same chart. 
-
-function showResults(){
+ 
+function trackResults(){
   var ctx = document.getElementById('analytics').getContext('2d');
   var nameArray = [];
   var clickedCount = [];
@@ -198,7 +185,7 @@ function showResults(){
       var itemSelection = SurveyItems.productList[i];
       nameArray.push(itemSelection.product);
       clickedCount.push(itemSelection.clicks);
-      // veiwCount.push(itemSelection.views);
+      viewCount.push(itemSelection.views);
     }
 
   new Chart(ctx, {
@@ -207,24 +194,76 @@ function showResults(){
         labels: nameArray,
         datasets: [{
             label: 'Times Clicked',
-            data: clickedCount,
+            data: viewCount,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+                
+                
             ],
             borderWidth: 1
+            },{
+            
+              type: 'line',
+              label: 'viewCount',
+              data: clickedCount,
+              backgroundColor: [
+                'rgb(0, 216, 255)'
+              ],
+              borderColor: [
+                'rgb(0, 216, 255'
+              ],
+              pointBackgroundColor:[
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)',
+                'rgb(229, 0, 255)'
+              ],
+              fill: false
+              
+              
         }]
     },
     options: {
